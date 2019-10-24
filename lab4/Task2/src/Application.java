a64a9680ca3c8e554c9ba31da91bc3d8458902e8
Jiklopo <itima.com@gmail.com>
2019-10-24 18:05:06 +0600

    Task2, Maybe should work on exceptions more

--- lab4/Task2/src/Application.java
+++ lab4/Task2/src/Application.java
@@ -1,10 +1,17 @@
+import java.io.IOException;
+
 import CourseConstructor.CourseConstructor;
 public class Application {
 
 	public static void main(String[] args) 
 	{
 		CourseConstructor courseConstructor = new CourseConstructor();
-		courseConstructor.run();
+		try {
+			courseConstructor.run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- lab4/Task2/src/CourseConstructor/CourseConstructor.java
+++ lab4/Task2/src/CourseConstructor/CourseConstructor.java
@@ -10,9 +10,24 @@ public class CourseConstructor {
 	private ArrayList<Instructor> instructors;
 	
 	{
-		courses = new ArrayList<Course>();
-		textbooks = new ArrayList<Textbook>();
-		instructors = new ArrayList<Instructor>();
+		try
+		{
+			ObjectInputStream txtbks = new ObjectInputStream(new FileInputStream("src\\resources\\textbooks.out"));
+			ObjectInputStream insts = new ObjectInputStream(new FileInputStream("src\\resources\\instructors.out"));
+			ObjectInputStream crss = new ObjectInputStream(new FileInputStream("src\\resources\\courses.out"));
+			textbooks = (ArrayList<Textbook>)txtbks.readObject();
+			instructors = (ArrayList<Instructor>)insts.readObject();
+			courses = (ArrayList<Course>)crss.readObject();
+			txtbks.close();
+			insts.close();
+			crss.close();
+		}
+		catch(Exception e)
+		{
+			courses = new ArrayList<Course>();
+			textbooks = new ArrayList<Textbook>();
+			instructors = new ArrayList<Instructor>();
+		}
 	}
 	
 	public CourseConstructor()
@@ -28,38 +43,77 @@ public class CourseConstructor {
 	}
 	
 	
-	public void run()
+	public void run() throws IOException
 	{
-		try {
-			adminLogin("Vasek");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		out.println("Welcome to Course App by Jiklopo corp.!");
+		String choice;
+		out.println("Type:\n"
+				+ "1 to enter as a guest\n"
+				+ "2 to enter as an admin.");
+		choice = in.readLine();
+		if(choice.equals("1"))
+			userMode();
+		else if(choice.equals("2"))
+		{
+			out.print("Login:");
+			out.flush();
+			String username = in.readLine();
+			out.print("Password:");
+			out.flush();
+			String password = in.readLine();
+			if(checkLogin(username, password))
+				adminMode(username);
+			else 
+				out.println("Wrong username or password!");
+		}
+		out.println("Goodbye!");
+		out.close();
+		in.close();
+	}
+	
+	private void userMode() throws IOException
+	{
+		String greeting = "\nType:\n"
+				+ "1 to list all Textbooks\n"
+				+ "2 to list all Instructors\n"
+				+ "3 to list all Courses\n"
+				+ "0 to quit";
+		String choice = "a";
+		while(!choice.equals("0"))
+		{
+			out.println(greeting);
+			choice = in.readLine();
+			if(choice.equals("1"))
+				listObjects(textbooks.toArray());
+			else if(choice.equals("2"))
+				listObjects(instructors.toArray());
+			else if(choice.equals("3"))
+				listObjects(courses.toArray());
 		}
 	}
 	
-	private void adminLogin (String username) throws IOException
+	private void adminMode (String username) throws IOException
 	{
-		String choice = "a";
 		out.println("Welcome, " + username);
-		
-		String greeting = "Type:\n"
+		String greeting = "\nType:\n"
 				+ "1 to add Textbook\n"
 				+ "2 to add Instructor\n"
 				+ "3 to add a Course\n"
 				+ "4 to list all Textbooks\n"
 				+ "5 to list all Instructors\n"
 				+ "6 to list all Courses\n"
+				+ "7 to delete a Textbook\n"
+				+ "8 to delete an Insturctor\n"
+				+ "9 to delete a Course\n"
 				+ "0 to quit";
 		
-		while(choice != null)
+		String choice = "a";
+		while(!choice.equals("0"))
 		{
 			out.println(greeting);
 			choice = in.readLine();
 			String logs = java.time.LocalDateTime.now() + " " + username + " ";
-			if(choice.equals("0"))
-				break;
-			else if(choice.equals("1"))
+			if(choice.equals("1"))
 			{
 				out.println("Enter isbn, title and author on separate lines");
 				String isbn = in.readLine();
@@ -79,7 +133,7 @@ public class CourseConstructor {
 				String email = in.readLine();
 				Instructor inst = new Instructor(firstName, lastName, department, email);
 				instructors.add(inst);
-				logs += "added instructor" + inst;
+				logs += "added instructor " + inst;
 			}
 			
 			else if(choice.equals("3"))
@@ -98,9 +152,12 @@ public class CourseConstructor {
 				}
 				
 				out.print("Enter course name: ");
+				out.flush();
 				String courseName = in.readLine();
+				out.println("Choose a textbook for the course");
 				listObjects(instructors.toArray());
 				int instChoice = Integer.parseInt(in.readLine());
+				out.println("Choose an instructor for the course");
 				listObjects(textbooks.toArray());
 				int txtbkChoice = Integer.parseInt(in.readLine());
 				Course course = new Course(courseName, textbooks.get(txtbkChoice), instructors.get(instChoice));
@@ -126,6 +183,30 @@ public class CourseConstructor {
 				logs += "checked courses.";
 			}
 			
+			else if(choice.equals("7"))
+			{
+				if(!textbooks.isEmpty())
+					logs += deleteSmthn(textbooks.toArray(), "Textbook");
+				else
+					logs += "tried to delete a Textbook";
+			}
+			
+			else if(choice.equals("8"))
+			{
+				if(!instructors.isEmpty())
+					logs += deleteSmthn(instructors.toArray(), "Instructor");
+				else
+					logs += "tried to delete an Instructor";
+			}
+			
+			else if(choice.equals("9"))
+			{
+				if(!courses.isEmpty())
+					logs += deleteSmthn(courses.toArray(), "Course");
+				else
+					logs += "tried to delete a Course";
+			}
+			
 			try {writeLogs(logs);}
 			catch(IOException e) {out.println("File logs.txt damaged or does not exist");}
 		}
@@ -138,6 +219,16 @@ public class CourseConstructor {
 		BufferedWriter logWriter = new BufferedWriter(new FileWriter("src\\resources\\logs.txt", true));
 		logWriter.write(logs + "\n");
 		logWriter.close();
+	}
+	
+	private String deleteSmthn(Object[] list, String cls) throws NumberFormatException, IOException
+	{
+		out.println("Choose a %s to delete:".format(cls));
+		listObjects(list);
+		int indexChoice = Integer.parseInt(in.readLine());
+		String logs = "deleted " + textbooks.get(indexChoice);
+		textbooks.remove(indexChoice);
+		return logs;
 	}
 	
 	private void listObjects(Object[] list)
@@ -153,10 +244,20 @@ public class CourseConstructor {
 	{
 		ObjectOutputStream txtbks = new ObjectOutputStream(new FileOutputStream("src\\resources\\textbooks.out", false));
 		ObjectOutputStream insts = new ObjectOutputStream(new FileOutputStream("src\\resources\\instructors.out", false));
-		ObjectOutputStream courses = new ObjectOutputStream(new FileOutputStream("src\\resources\\courses.out", false));
+		ObjectOutputStream crss = new ObjectOutputStream(new FileOutputStream("src\\resources\\courses.out", false));
 		txtbks.writeObject(textbooks);
 		insts.writeObject(instructors);
-		courses.writeObject(courses);
-		//serialize all arraylists
+		crss.writeObject(courses);
+		txtbks.close();
+		insts.close();
+		crss.close();
+	}
+	
+	private boolean checkLogin(String username, String password) throws FileNotFoundException, IOException
+	{
+		BufferedReader pass = new BufferedReader(new FileReader("src\\CourseConstructor\\admin.txt"));
+		String login = pass.readLine();
+		int hashPassword = Integer.parseInt(pass.readLine());
+		return login.equals(username) && hashPassword == password.hashCode();
 	}
 }
